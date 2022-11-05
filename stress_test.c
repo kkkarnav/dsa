3b4c72ebb31fca7bc7914e3a4b690e40b0626a81
Karnav <karnavpopat@gmail.com>
2022-11-05 17:53:31 +0530

    feat: stress test

--- stress_test.c
+++ stress_test.c
@@ -1,27 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-
-int main() {
-
-   int count = 1;
-   float increment = 1.0;
-   float max = 10.0;
-
-   for (int count = 1; count <= 10; count++) {
-
-		float increment = increment/10.0;
-		float max = max/10.0;
-
-		float i = 0.0;
-		int counter = 0;
-		for (i = 0.0; counter < 10; i += increment) {
-			counter += 1;
-		}
-
-		printf("%d \n", count);
-	}
-	
-	return 0;
+void main() {
+    for (float a=0.0; a<0.0000001; a=a+0.00000001) {
+        printf("%d, ", a);
+    }
 }
