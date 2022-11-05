cc83e8a71b510a83d8eaac8f103d839fe52fc187
Karnav <karnavpopat@gmail.com>
2022-11-05 17:54:02 +0530

    feat: stress test

--- stress_test.c
+++ stress_test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void main() {
-    for (float a=0.0; a<0.0000001; a=a+0.00000001) {
-        printf("%d, ", a);
+    for (float i=0.0; i<0.0000001; i+=0.00000001) {
+        printf("%d, ", i);
     }
 }
